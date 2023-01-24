7ff7793c0a5ce0e421648fceec1974be26bcea53
Joel Augusto <joelaugusto97@gmail.com>
2023-01-24 17:37:07 +0200

    initial commit

---
+++ src/main/java/com/mycompany/projectoclinica/Projectoclinica.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projectoclinica;
+
+import com.mycompany.projectoclinica.views.ClinicFrame;
+
+/**
+ *
+ * @author Joel Augusto
+ */
+public class Projectoclinica {
+
+    public static void main(String[] args) {
+      
+    }
+}
